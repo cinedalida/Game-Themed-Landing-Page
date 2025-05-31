d6297a2af6fd6276eb7839c57044853b6582f999
Francine Dalida <francine.dalida@gmail.com>
2025-05-31 18:10:36 +0800

    Added: Header and Footer

---
+++ Basic-Login-Signup/frontend/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import "./footer.css";
+
+export function Footer() {
+  return (
+    <section className="footer">
+      <div className="footer__content">
+        <p className="subject">Application Development</p>
+        <p className="myname">Francine Ysabel B. Dalida</p>
+      </div>
+    </section>
+  );
+}
---
+++ Basic-Login-Signup/frontend/src/components/Header.jsx
@@ -0,0 +1,22 @@
+import "./header.css";
+import { Link } from "react-router-dom";
+
+export function Header() {
+  return (
+    <header className="header">
+      <nav className="navbar">
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
