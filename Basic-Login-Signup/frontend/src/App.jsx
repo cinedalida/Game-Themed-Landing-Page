41002cee77dd9cbe776b9d1a818655d6ac4eb7b1
Francine Dalida <francine.dalida@gmail.com>
2025-06-01 23:24:05 +0800

    Updated: Routing

--- Basic-Login-Signup/frontend/src/App.jsx
+++ Basic-Login-Signup/frontend/src/App.jsx
@@ -1,11 +1,39 @@
 import "./App.css";
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import { Header } from "./components/Header";
 import { Landing } from "./pages/Landing.jsx";
+import { Home } from "./pages/Home.jsx";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
 
 function App() {
   return (
     <>
-      <Landing />
+      <Router>
+        <AppContent />
+      </Router>
+    </>
+  );
+}
+
+function AppContent() {
+  const location = useLocation();
+  const isLanding = location.pathname === "/";
+
+  return (
+    <>
+      {!isLanding && <Header />}
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </>
   );
 }
